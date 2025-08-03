e04133408dfb689bd5fd1dd4692b89a60631765f
Yunus Yildirim <coderynsyldrm@gmail.com>
2025-08-03 15:15:04 +0300

    phone book app creating is started with base improvements

---
+++ src/Shared/Models/Contact.cs
@@ -0,0 +1,26 @@
+﻿namespace Shared.Models
+{
+  public class Contact
+  {
+    public Guid Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Company { get; set; }
+    public List<ContactInfo> ContactInfos { get; set; } = new();
+  }
+
+  public class ContactInfo
+  {
+    public Guid Id { get; set; }
+    public Guid ContactId { get; set; }
+    public ContactInfoType InfoType { get; set; }
+    public string InfoContent { get; set; }
+  }
+
+  public enum ContactInfoType
+  {
+    PhoneNumber,
+    EmailAddress,
+    Location
+  }
+}
---
+++ src/Shared/Models/Report.cs
@@ -0,0 +1,23 @@
+﻿namespace Shared.Models
+{
+  public class Report
+  {
+    public Guid Id { get; set; }
+    public DateTime RequestedDate { get; set; }
+    public ReportStatus Status { get; set; }
+    public List<LocationStatistic> Statistics { get; set; } = new();
+  }
+
+  public class LocationStatistic
+  {
+    public string Location { get; set; }
+    public int PersonCount { get; set; }
+    public int PhoneNumberCount { get; set; }
+  }
+
+  public enum ReportStatus
+  {
+    Preparing,
+    Completed
+  }
+}
